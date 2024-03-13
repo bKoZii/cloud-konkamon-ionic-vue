f657ba8373269c2376259e3bb307a500077a5322
Konkamon Sion <bigkonkamon@gmail.com>
2024-03-13 20:07:35 +0700

    Moved Modal from `MemberPage.vue`

---
+++ src/components/addMemberModal.vue
@@ -0,0 +1,154 @@
+<template>
+
+  <!-- <ion-modal ref="modal" :can-dismiss="canDismiss" :presenting-element="page.$el" mode="ios"
+      :keep-contents-mounted="false"> -->
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>เพิ่มข้อมูลสมาชิก</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="dialogDismiss()">
+          <ion-icon :icon="close"></ion-icon></ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    <ion-item>
+      <ion-select label="คำนำหน้า" value="นาย" v-model="member.title" cancel-text="ยกเลิก" ok-text="บันทึก"
+        justify="space-between" :interface-options="customAlertOptions">
+        <ion-select-option value="นาย" aria-selected="true">นาย</ion-select-option>
+        <ion-select-option value="นาง">นาง</ion-select-option>
+        <ion-select-option value="นางสาว">นางสาว</ion-select-option>
+      </ion-select>
+    </ion-item>
+    <ion-item>
+      <ion-input label="ชื่อ" type="text" v-model="member.fName" placeholder="กรุณาป้อนข้อมูล"
+        label-placement="floating"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="นามสกุล" type="text" v-model="member.lName" placeholder="กรุณาป้อนข้อมูล"
+        label-placement="floating"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="อีเมลล์" type="email" v-model="member.email" placeholder="กรุณาป้อนข้อมูล"
+        label-placement="floating"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="เบอร์โทร" type="tel" v-model="member.phone" placeholder="กรุณาป้อนข้อมูล"
+        label-placement="floating"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-input label="Line ID" type="text" v-model="member.lineID" fill="outline" label-placement="floating"
+        placeholder="กรุณาป้อนข้อมูล"></ion-input>
+    </ion-item>
+    <ion-item>
+      <ion-textarea label="ที่อยู่" placeholder="กรุณาป้อนข้อมูล" label-placement="floating"></ion-textarea>
+    </ion-item>
+    <ion-button expand="block" class="ion-margin-top" @click="sendData()"
+      :disabled="!member.fName || !member.lName">บันทึก</ion-button>
+    <ion-button expand="block" :disabled="isMemberDataEmpty" @click="clearMember()">Reset Form</ion-button>
+
+    <ion-card>
+      <ion-card-header>
+        <h3>Two-Way Binding Test{{ member.dateAdded }}
+        </h3>
+      </ion-card-header>
+      <ion-card-content>
+        <ion-list>
+          <ion-item>
+            <ion-label>ชื่อเต็ม</ion-label>
+            {{ member.title + " " + member.fName + " " + member.lName }}
+          </ion-item>
+          <ion-item>
+            <ion-label>E-Mail</ion-label>
+            {{ member.email }}
+          </ion-item>
+          <ion-item>
+            <ion-label>เบอร์โทร</ion-label>
+            {{ member.phone }}
+          </ion-item>
+          <ion-item>
+            <ion-label>LINE ID</ion-label>
+            {{ member.lineID }}
+          </ion-item>
+        </ion-list>
+      </ion-card-content>
+    </ion-card>
+  </ion-content>
+  <!-- </ion-modal> -->
+</template>
+<script setup lang="ts">
+import { addMember } from "@/firebaseConfig";
+import { memberToast } from "@/utilFunctions";
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+  modalController,
+} from "@ionic/vue";
+import { close } from "ionicons/icons";
+import { computed, ref } from 'vue';
+import { Timestamp } from "firebase/firestore";
+
+const member = ref({
+  title: "",
+  fName: "",
+  lName: "",
+  lineID: "",
+  email: "",
+  phone: "",
+  dateAdded: ""
+});
+const customAlertOptions = {
+  header: 'คำนำหน้า',
+  subHeader: 'กรุณาเลือกเพียงหนึ่งรายการ',
+  translucent: true,
+};
+
+const sendData = async () => {
+  member.value.dateAdded = Timestamp.now().toMillis().toString()
+  const memberObject = member.value; // แปลงจาก Ref ของ Vue ให้เป็น Object ของ JavaScript
+
+  dialogDismiss();
+
+  try {
+    addMember({ ...memberObject }).then(async () => {
+      await memberToast("เพิ่มข้อมูลสมาชิกสำเร็จ");
+    });
+  } catch (e: any) {
+    console.error("Error Adding Member: ", e);
+    await memberToast(e)
+  }
+};
+
+const dialogDismiss = () => modalController.dismiss();
+
+const clearMember = () => {
+  const clearedMember = {
+    ...member.value,
+    ...Object.keys(member.value).reduce(
+      (acc, key) => ({ ...acc, [key]: "" }),
+      {}
+    ),
+  };
+  member.value = clearedMember;
+};
+
+const isMemberDataEmpty = computed(() => {
+  return Object.values(member.value).every((value) => value.trim() === "");
+});
+
+</script>
