63c35fa9168bc758ea3a8718a6263c55ff2c84da
Konkamon Sion <bigkonkamon@gmail.com>
2024-03-14 09:38:05 +0700

    Feat: Make memberToast accept color value

--- src/utilFunctions.ts
+++ src/utilFunctions.ts
@@ -1,14 +1,13 @@
 import { toastController } from "@ionic/vue";
 import { checkmarkCircle } from "ionicons/icons";
 
-export const memberToast = async (msg: string) => {
+export const memberToast = async (msg: string, color: string = 'success') => {
   const toast = await toastController.create({
     message: msg,
     duration: 1500,
     position: "top",
-    color: "success",
+    color: color,
     icon: checkmarkCircle,
   });
   await toast.present();
-  console.log("tst")
 };
