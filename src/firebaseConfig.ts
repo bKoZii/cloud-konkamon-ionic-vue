8c5b69ce96ce69661ec44565666d03b7970d5237
Konkamon Sion <bigkonkamon@gmail.com>
2024-03-14 09:25:24 +0700

    Feat: Now returning a Result Message

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -8,18 +8,21 @@ export const firebaseApp = initializeApp(env.firebase);
 const db = getFirestore(firebaseApp);
 export const memberRef = collection(db, 'members');
 
-export const addMember = async (data: any) => {
-  try {
-    await addDoc(memberRef, {...data});
-  } catch (e:any) {
-    console.error("Error adding document: ", e);
-  }
+export const addMember = async (data: any): Promise<string> => {
+    try {
+      await addDoc(memberRef, data);
+     return ("เพิ่มข้อมูลสมาชิกสำเร็จ")
+    } catch (e:any) {
+      throw new Error("Error adding document: " + e.message);
+    }
 };
 
-export const deleteMember = async (id: any) => {
+export const deleteMember = async (id: any): Promise<string> => {
   try {
-    await deleteDoc(doc(memberRef,id))
-  } catch (e:any) {
-    console.error("Error adding document: ", e);
+     await deleteDoc(doc(memberRef, id));
+     return "ลบข้อมูลสมาชิกสำเร็จ";
+  } catch (e: any) {
+     console.error("Error deleting document: ", e);
+     throw new Error("Error: " + e.message);
   }
-}
+ };
