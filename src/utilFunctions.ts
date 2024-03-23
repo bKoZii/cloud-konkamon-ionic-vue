efd8decda80753591dad5ea18ea05dab48531fe5
Konkamon Sion <bigkonkamon@gmail.com>
2024-03-23 21:25:45 +0700

    Moved Default Color to `color` Property

--- src/utilFunctions.ts
+++ src/utilFunctions.ts
@@ -1,12 +1,12 @@
 import { toastController } from "@ionic/vue";
 import { checkmarkCircle } from "ionicons/icons";
 
-export const memberToast = async (msg: string, color: string = 'success') => {
+export const memberToast = async (msg: string, color?: string) => {
   const toast = await toastController.create({
     message: msg,
     duration: 1500,
     position: "top",
-    color: color,
+    color: color || "success",
     icon: checkmarkCircle,
   });
   await toast.present();
