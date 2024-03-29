b25e9ae2fd3511ae3fae274c63f34a18159d439e
Konkamon Sion <bigkonkamon@gmail.com>
2024-03-29 11:01:42 +0700

    Chore: Changed alt/My Details

--- src/components/home/myInfo.vue
+++ src/components/home/myInfo.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <ion-card>
-      <ion-img alt="Silhouette of mountains" src="/me-rmutsv-square.jpg" />
+      <ion-img src="/me-rmutsv-square.webp" alt="Konkamon Sion" />
       <ion-card-header>
         <ion-card-title>นาย กรกมล ศรีอ่อน</ion-card-title>
         <ion-card-subtitle>บิ๊ก</ion-card-subtitle>
       </ion-card-header>
       <ion-card-content>
-        <p>นักศึกษา คณะวิทยาศาสตร์และเทคโนโลยี สาขาเทคโนโลยีสารสนเทศ</p>
+        <p>นักศึกษา มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย คณะวิทยาศาสตร์และเทคโนโลยี แผนกเทคโนโลยีสารสนเทศ</p>
       </ion-card-content>
       <ion-button fill="clear" href="https://github.com/bKoZii"><ion-icon :icon="logoGithub"></ion-icon></ion-button>
     </ion-card>
